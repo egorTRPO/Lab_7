b69ef669a8aaf975e7e7b91da9836b62a2f3a5e4
Secure <ezidovic@gmail.com>
2023-05-26 17:38:55 +0300

    Changed file Note.kt

--- app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
+++ app/src/main/java/com/topic2/android/notes/ui/components/Note.kt
@@ -5,12 +5,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -19,13 +25,29 @@ import com.topic2.android.notes.theme.rwGreen
 
 @Composable
 fun Note() {
-    Row(modifier = Modifier.fillMaxWidth()) {
-        Box(
+    val backgroundShape: Shape = RoundedCornerShape(4.dp)
+    Row(
             modifier = Modifier
-                .size(40.dp)
-                .background(rwGreen)
+                .padding(8.dp)
+                .shadow(1.dp, backgroundShape)
+                .fillMaxWidth()
+                .heightIn(min = 64.dp)
+                .background(Color.White, backgroundShape)
+
+    ) {
+        NoteColor(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(start = 16.dp, end = 16.dp),
+            color = rwGreen ,
+            size = 40.dp,
+            border = 1.dp
         )
-        Column(modifier = Modifier.weight(1f)) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .align(Alignment.CenterVertically)
+        ) {
             Text(
                 text = stringResource(id = R.string.header),
                 maxLines = 1
@@ -38,7 +60,9 @@ fun Note() {
         Checkbox(
             checked = false,
             onCheckedChange = {},
-            modifier = Modifier.padding(8.dp)
+            modifier = Modifier
+                .padding(8.dp)
+                .align(Alignment.CenterVertically)
         )
     }
 }
