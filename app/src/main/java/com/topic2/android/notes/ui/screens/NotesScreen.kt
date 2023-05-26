346bb3df3f2b082b27fa3c0b90deb1a43eef3d90
Secure <ezidovic@gmail.com>
2023-05-26 22:05:27 +0300

    Add function rememberScaffoldState and rememberDrawerState

--- app/src/main/java/com/topic2/android/notes/ui/screens/NotesScreen.kt
+++ app/src/main/java/com/topic2/android/notes/ui/screens/NotesScreen.kt
@@ -3,14 +3,21 @@ package com.topic2.android.notes.ui.screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.DrawerState
+import androidx.compose.material.DrawerValue
 import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.topic2.android.notes.R
@@ -21,6 +28,24 @@ import com.topic2.android.notes.ui.components.Note
 import com.topic2.android.notes.ui.components.TopAppBar
 import com.topic2.android.notes.viewmodel.MainViewModel
 import kotlinx.coroutines.launch
+
+@Composable
+fun rememberScaffoldState(
+    drawerState: DrawerState = rememberDrawerState(DrawerValue.Closed),
+    snackbarHostState: SnackbarHostState = remember { SnackbarHostState() }
+): ScaffoldState = remember{
+    ScaffoldState(drawerState, snackbarHostState)
+}
+@Composable
+fun rememberDrawerState(
+    initialValue: DrawerValue,
+    confirmStateChange: (DrawerValue) -> Boolean = { true }
+): DrawerState
+{
+    return rememberSaveable(saver = DrawerState.Saver(confirmStateChange)) {
+        DrawerState(initialValue,confirmStateChange)
+    }
+}
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
